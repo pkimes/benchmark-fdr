30f9ae786f052e3d6df17a5048297b3c92c772ea
Patrick Kimes <patrick.kimes@gmail.com>
2017-09-14 09:11:32 -0400

    Move simulation helper functions to separate file

---
+++ simulation_studies/R/simulation-helpers.R
@@ -0,0 +1,162 @@
+#' Simulate Collection of T-Tests
+#'
+#' Function for simulating a large set of (independent) t-tests. Simulation is performed
+#' at the "raw data" level. Test results are returned as a data.frame with one row per
+#' test. 
+#' 
+#' @param m number of hypothesis tests 
+#' @param pi0 proportion of true null hypotheses
+#' @param effect_size expected mean difference
+#' @param n_samples number of total samples, should be multiple of `n_groups` (default = 20)
+#' @param n_groups number of groups in comparison (default = 2)
+#' @param seed integer seed for random number generator, ignored if NULL (default = NULL) 
+#'
+#' @return
+#' data.frame of test results for `m` simulated data sets, with columns:
+#' * `H`: 0/1 indicator whether data simulated under null (0) or alternative (1)
+#' * `t_stat`: t-test statistic
+#' * `t_dm`: group mean difference
+#' * `t_pval`: t-test p-value
+#' * `sd`: standard deviation across all samples
+#' * `SE`: standard error across all samples
+#'
+#' @md
+#' @importFrom genefilter rowtests
+#' @importFrom genefilter rowSds
+#' @author Stephanie Hicks
+du_ttest_sim <- function(m, pi0, effect_size, n_samples = 20, n_groups = 2, seed = NULL) {
+    if (!is.null(seed)) {
+        set.seed(seed)
+    }
+
+    m0 <- ceiling(m*pi0)
+    false_nulls <- sample(1:m, m - m0)
+    
+    z_table <- matrix(rnorm(n_samples*m), ncol=n_samples)
+    z_table[false_nulls, (n_samples / 2 + 1):n_samples] <- matrix(rnorm(n_samples/2*(m-m0), effect_size), ncol=n_samples/2)
+
+    H <- rep(0, m)
+    H[false_nulls] <- 1
+    gF <- factor(rep(1:n_groups, each=n_samples/n_groups))
+    t_test <- genefilter::rowttests(z_table, gF)
+    sds <- genefilter::rowSds(z_table) # pooled var reduces to unpooled var b/c same sample sizes across groups 
+    SE <- sds / sqrt(n_samples)
+    simDf <- data.frame(H = H, t_stat = t_test$statistic, t_dm = t_test$dm, 
+                        t_pval = t_test$p.value, sd = sds, SE = SE)
+    return(simDf)
+}
+
+
+#' Calculate Performance Metrics for Adjusted P-Values
+#'
+#' Function for evaluating the performance of adjusted p-values calculated
+#' for a set of t-tests simulated using `du_ttest_sim`. Metrics 
+#' calculated for the adjusted p-values are returned in a data.frame with
+#' a single row. Each column contains a separate performance metric.
+#' 
+#' @param sim data.frame output from `du_ttest_sim` containing details of 
+#'        simulated hypothesis tests
+#' @param adj_p vector of adjusted p-values with length equal to the number
+#'        of rows in `sim`
+#' @param alpha significance threshold for adjusted p-values
+#'
+#' @return
+#' data.frame with single row summarizing performance of adjusted p-values
+#' using various metrics, each in a separate column:
+#' * `n_rejects`: total number of rejections
+#' * `FWER`: family-wise error rate
+#' * `FPR`: false positive rate
+#' * `FDP`: false discovery proportion
+#' * `alpha`: alpha input parameter
+#'
+#' @md
+#' @author Stephanie Hicks
+calculate_test_stats <- function(sim, adj_p, alpha) {
+    ## check that dimensions agree
+    stopifnot(length(adj_p) == nrow(sim))
+    
+    rejected <- adj_p <= alpha
+    rjs <- sum(rejected) # number of rejected nulls
+    false_rjs <- sum(sim$H == 0 & rejected) # number of false rejected nulls
+    rj_ratio <- rjs/nrow(sim)
+    FDP <- ifelse(rjs == 0, 0, false_rjs / rjs) # False Discovery Proportion
+    power <- ifelse(sum(sim$H) == 0, NA, 
+                    sum(sim$H == 1 & rejected) / sum(sim$H == 1)) # power
+    FPR <- sum(sim$H == 0 & rejected) / sum(sim$H == 0)
+    FWER <- as.numeric(false_rjs > 0)
+    df <- data.frame(n_rejects = rjs, FWER=FWER, FPR=FPR, 
+                     FDP=FDP, alpha = alpha) #  power=power, 
+    return(df)
+}
+
+
+
+#' Simulation Evaluation
+#'
+#' Wrapper function for computing adjusted p-values (or comparable values) for
+#' all methods in benchmkaring study and computing evaluation metrics against
+#' expected values at various FDR thresholds.
+#' 
+#' @param sim_df data.frame output from `du_ttest_sim` containing details of 
+#'        simulated hypothesis tests
+#' @param alphas vector of FDR cutoffs between 0, 1
+#'
+#' @return
+#' data.frame with evaluation metrics for each method at the specified alpha
+#' thresholds. Should have number of rows equal to `length(alphas) * (n_methods)`,
+#' where `n_methods` is the number of different methods (or methods and parameters)
+#' in the benchmark study.
+#'
+#' @md
+#' @author Stephanie Hicks
+sim_runner <- function(sim_df, alphas) { 
+
+    ## Bonferroni 
+    adj_p <- p.adjust(sim_df$t_pval, method="bonferroni")
+    df.bonf <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+
+    ## BH
+    adj_p <- p.adjust(sim_df$t_pval, method="BH")
+    df.bh <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+    
+    ## qvalue (Storey)
+    adj_p <- qvalue::qvalue(p=sim_df$t_pval)$qvalues
+    df.qvalue <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+    
+    ## IHW (Wolfgang)
+    ihw_fdr <- IHW::ihw(pvalues = sim_df$t_pval, covariates = sim_df$sd, alpha =  0.1)
+    adj_p <- IHW::adj_pvalues(ihw_fdr)
+    df.ihw <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+
+    ## ash (Stephens)
+    beta.ash = ashr::ash(betahat = sim_df$t_dm, sebetahat = sim_df$SE)
+    adj_p <- ashr::get_svalue(beta.ash)
+    df.ash <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+    
+    ## swfdr::lm_pi0 (Boca-Leek) **think about multiple covariates** or **correlation structure**
+    pi0x <- swfdr::lm_pi0(pValues = sim_df$t_pval, X = sim_df$sd, smooth.df =3)
+    adj_p <- p.adjust(sim_df$t_pval, method="BH")*pi0x$pi0
+    df.BL <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+    
+    ## Scott et al. (2015) (FDRreg)
+    adj_p <- scott_fdrreg_hickswrapper(unadj_p = sim_df$t_pval, filterstat = sim_df$sd, df=3)
+    df.scott <- plyr::ldply(alphas, function(a){
+        calculate_test_stats(sim=sim_df, adj_p = adj_p, alpha = a) })
+    
+    ## locfdr (Cai)
+
+    ## Summary table of FDP for each method at each alpha
+    rbind(data.frame(df.bonf, "method" = "bonferroni"), 
+          data.frame(df.bh, "method" = "bh"), 
+          data.frame(df.qvalue, "method" = "qvalue"), 
+          data.frame(df.ihw, "method" = "ihw"), 
+          data.frame(df.ash, "method" = "ash"), 
+          data.frame(df.BL, "method" = "boca-leek"), 
+          data.frame(df.scott, "method" = "scott"))
+}
