b3f978446a13efb6bbfe80c82a4ba61b4cb347c1
Patrick Kimes <patrick.kimes@gmail.com>
2017-11-13 19:15:34 -0500

    Replace 'scott_fdrreg_hickswrapper' w/ call to FDRreg in benchDesign

--- datasets/R/common-BenchDesign.R
+++ datasets/R/common-BenchDesign.R
@@ -28,7 +28,7 @@
 #' 
 #' @author Patrick Kimes
 
-initializeBenchDesign <- function(){
+initializeBenchDesign <- function() {
   ## ###########################################################################
   ## check for necessary packages and load them if they aren't present
   ## ###########################################################################
@@ -48,24 +48,24 @@ initializeBenchDesign <- function(){
   bd <- BenchDesign()
   ## unadjusted p-values
   bd %<>% addBMethod("unadjusted",
-                   function(p) { p },
-                   p = pval)
+                     function(p) { p },
+                     p = pval)
   ## Bonferonni correction
   bd %<>% addBMethod("bonf",
-                   p.adjust,
-                   p = pval, method = "bonferroni")
+                     p.adjust,
+                     p = pval, method = "bonferroni")
   ## Benjamini-Hochberg correction
   bd %<>% addBMethod("bh",
-                   p.adjust,
-                   p = pval, method = "BH")
+                     p.adjust,
+                     p = pval, method = "BH")
   ## Storey's q-value
   bd %<>% addBMethod("qvalue",
-                   qvalue::qvalue,
-                   function(x) { x$qvalues },
-                   p = pval)
+                     qvalue::qvalue,
+                     function(x) { x$qvalues },
+                     p = pval)
   ## IHW (w/ varying alpha threshold) 
   for (ia in seq(0.01, 0.10, by=0.01)) {
-      bd %<>% addBMethod(paste0("ihw-a", sprintf("%02g", ia*100)),
+    bd %<>% addBMethod(paste0("ihw-a", sprintf("%02g", ia*100)),
                        IHW::ihw,
                        IHW::adj_pvalues,
                        pvalues = pval, covariates = ind_covariate,
@@ -73,32 +73,36 @@ initializeBenchDesign <- function(){
   }
   ## Stephens' ASH
   bd %<>% addBMethod("ashs",
-                   ashr::ash,
-                   ashr::get_svalue,
-                   betahat = effect_size, sebetahat = SE)
+                     ashr::ash,
+                     ashr::get_svalue,
+                     betahat = effect_size, sebetahat = SE)
   ## Boca-Leek (w/ varying smoothing degrees of freedom)
   for (idf in 2:5) {
       bd %<>% addBMethod(paste0("bl-df", sprintf("%02g", idf)), 
-                       swfdr::lm_pi0,
-                       function(x) { x$pi0 * p.adjust(pval, method = "BH") },
-                       pValues = pval, X = ind_covariate,
-                       smooth.df = UQ(idf))
+                         swfdr::lm_pi0,
+                         function(x) { x$pi0 * p.adjust(pval, method = "BH") },
+                         pValues = pval, X = ind_covariate,
+                         smooth.df = UQ(idf))
   }
   ## Cai's local FDR
   bd %<>% addBMethod("lfdr",
-                   clfdr_hickswrapper,
-                   unadj_p = pval, groups = IHW::groups_by_filter(ind_covariate, 20))
+                     clfdr_hickswrapper,
+                     unadj_p = pval, groups = IHW::groups_by_filter(ind_covariate, 20))
   ## Scott's FDR regression w/ theoretical null
   bd %<>% addBMethod("scott-theoretical",
-                   scott_fdrreg_hickswrapper,
-                   zscores = qnorm(exp(log(pval)-log(2)), lower.tail=FALSE) * sign(test_statistic),
-                   filterstat = ind_covariate, df = 3, lambda = 0.01,
-                   nulltype = 'theoretical')
+                     FDRreg::FDRreg,
+                     function(x) { x$FDR },
+                     z = qnorm(exp(log(pval) - log(2)), lower.tail=FALSE) * sign(test_statistic),
+                     features = model.matrix( ~  splines::bs(ind_covariate, df = 3) - 1),
+                     nulltype = 'theoretical',
+                     control = list(lambda = 0.01))
   ## Scott's FDR regression w/ empirical null
   bd %<>% addBMethod("scott-empirical",
-                   scott_fdrreg_hickswrapper,
-                   zscores = qnorm(exp(log(pval)-log(2)), lower.tail=FALSE) * sign(test_statistic),
-                   filterstat = ind_covariate, df = 3, lambda = 0.01,
-                   nulltype = 'empirical')
+                     FDRreg::FDRreg,
+                     function(x) { x$FDR },
+                     z = qnorm(exp(log(pval) - log(2)), lower.tail=FALSE) * sign(test_statistic),
+                     features = model.matrix( ~  splines::bs(ind_covariate, df = 3) - 1),
+                     nulltype = 'empirical',
+                     control = list(lambda = 0.01))
   return(bd)
 }
