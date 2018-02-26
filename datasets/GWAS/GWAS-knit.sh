ad03989837d8066672fb4c2019758dda14764b8d
kdkorthauer <kdkorthauer@gmail.com>
2018-02-26 15:26:04 -0500

    Add plink module load to batch params

--- datasets/GWAS/GWAS-knit.sh
+++ datasets/GWAS/GWAS-knit.sh
@@ -4,14 +4,15 @@
 #SBATCH -N 1                        # All cores on one machine
 #SBATCH -p irizarry,serial_requeue  # Partition
 #SBATCH --mem 60000                 # Memory request 
-#SBATCH -t 0-5:00                   # Maximum execution time (D-HH:MM)
+#SBATCH -t 0-2:00                   # Maximum execution time (D-HH:MM)
 #SBATCH -o SLURM/render-%j.out  # Standard output
 #SBATCH -e SLURM/render-%j.err  # Standard error
  
+module load plink
 module load rstudio
 export SLURM_NTASKS
 export RSTUDIO_PANDOC="/n/sw/fasrcsw/apps/Core/rstudio/0.98.1103-fasrc01/bin/pandoc/"
 
 # change filename to Rmd to be knitted. ncores is passed in through environment
 # variable SLURM_NTASKS
-R -e "rmarkdown::render('/n/irizarryfs01_backed_up/kkorthauer/FDR/benchmark-fdr/datasets/GWAS/GWAS-SummarizedBenchmark.Rmd')"
+R -e "rmarkdown::render('GWAS.Rmd')"
