577e10e16f7a97c6f1c3700cbaf14e57776bb712
kdkorthauer <kdkorthauer@gmail.com>
2018-03-08 16:42:09 -0500

    Add covariate heatmaps. Use mean nonzero expression as covariate

--- datasets/scrnaseq/scrnaseq-knit.sh
+++ datasets/scrnaseq/scrnaseq-knit.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#SBATCH -J knit-sc4                  # A single job name for the array
-#SBATCH -n 6                         # Number of cores
-#SBATCH -N 1                         # All cores on one machine
-#SBATCH -p irizarry,serial_requeue   # Partition
-#SBATCH --mem 200G                   # Memory request 
-#SBATCH -t 0-20:00                   # Maximum execution time (D-HH:MM)
+#SBATCH -J knit-sc                  # A single job name for the array
+#SBATCH -n 8                        # Number of cores
+#SBATCH -N 1                        # All cores on one machine
+#SBATCH -p shared                   # Partition
+#SBATCH --mem 125G                  # Memory request 
+#SBATCH -t 0-6:00                   # Maximum execution time (D-HH:MM)
 #SBATCH -o render-%j.out
 #SBATCH -e render-%j.err
  
-module load rstudio
 export SLURM_NTASKS
 export RSTUDIO_PANDOC="/n/sw/fasrcsw/apps/Core/rstudio/0.98.1103-fasrc01/bin/pandoc/"
 
-# change filename to Rmd to be knitted. ncores is passed in through environment
-# variable SLURM_NTASKS
-R -e "rmarkdown::render('scrnaseq-human-scdd.Rmd')"
-R -e "rmarkdown::render('scrnaseq-human-mast.Rmd')"
-R -e "rmarkdown::render('scrnaseq-mouse-scdd.Rmd')"
-R -e "rmarkdown::render('scrnaseq-mouse-mast.Rmd')"
+# change filename to Rmd to be knitted
+# make sure number of cores used in Rmd matches sbatch param -n
+R -e "rmarkdown::render('scrnaseq-mouse-scdd.Rmd', clean=FALSE)"
+R -e "rmarkdown::render('scrnaseq-mouse-mast.Rmd', clean=FALSE)"
+R -e "rmarkdown::render('scrnaseq-human-scdd.Rmd', clean=FALSE)"
+R -e "rmarkdown::render('scrnaseq-human-mast.Rmd', clean=FALSE)"
