d4c40378c813192644c229b1c8f67623beb52ac4
kdkorthauer <kdkorthauer@gmail.com>
2018-03-14 12:10:58 -0400

    Add covariate line plots

--- datasets/GWAS/GWAS-knit.sh
+++ datasets/GWAS/GWAS-knit.sh
@@ -2,12 +2,12 @@
 #SBATCH -J knitGWAS                 # A single job name for the array
 #SBATCH -n 8                        # Number of cores
 #SBATCH -N 1                        # All cores on one machine
-#SBATCH -p irizarry,serial_requeue  # Partition
+#SBATCH -p shared                   # Partition
 #SBATCH --mem 60000                 # Memory request 
 #SBATCH -t 0-2:00                   # Maximum execution time (D-HH:MM)
-#SBATCH -o SLURM/render-%j.out  # Standard output
-#SBATCH -e SLURM/render-%j.err  # Standard error
- 
+#SBATCH -o render-%j.out  # Standard output
+#SBATCH -e render-%j.err  # Standard error
+
 module load plink
 export SLURM_NTASKS
 export RSTUDIO_PANDOC="/n/sw/fasrcsw/apps/Core/rstudio/0.98.1103-fasrc01/bin/pandoc/"
