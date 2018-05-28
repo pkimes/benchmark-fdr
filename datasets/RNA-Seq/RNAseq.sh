72358cc382debd7d2f74f9a73e31a6d5247991c9
kdkorthauer <kdkorthauer@gmail.com>
2018-05-28 15:09:57 -0400

    Update batch submit script for Centos7

--- datasets/RNA-Seq/RNAseq.sh
+++ datasets/RNA-Seq/RNAseq.sh
@@ -10,7 +10,7 @@
  
 ##module load rstudio
 export SLURM_NTASKS
-export RSTUDIO_PANDOC="/n/sw/fasrcsw/apps/Core/rstudio/0.98.1103-fasrc01/bin/pandoc/"
+export RSTUDIO_PANDOC="/n/helmod/apps/centos7/Core/rstudio/1.1.453-fasrc01/bin/pandoc"
 
 # change filename to Rmd to be knitted. ncores is passed in through environment
 # variable SLURM_NTASKS
